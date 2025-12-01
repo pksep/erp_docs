284054e70737e7aeadae880f77a9a6554edc78fb
kogeletey <kg@re128.org>
2025-12-01 20:45:19 +0300

    fix: docs to root

---
+++ .vitepress/config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'ERP Docs',
+  description: 'Центр помощи ERP Docs',
+  themeConfig: {
+    nav: [
+      { text: 'Документы', link: '/' },
+    ]
+  }
+})
